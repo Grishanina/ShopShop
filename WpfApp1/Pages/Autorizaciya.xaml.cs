d8b4876c7c2b144336d8871b03eef14b4eab93f3
Ксения <grishanina0404@mail.ru>
2022-10-30 18:48:08 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/Pages/Autorizaciya.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Autorizaciya.xaml
+    /// </summary>
+    public partial class Autorizaciya : Page
+    {
+        public Autorizaciya()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn2_Click(object sender, RoutedEventArgs e)
+        {
+			int p = pbpas.Password.GetHashCode();
+			
+            T_Users UsersObject = ClassBase.BD.T_Users.FirstOrDefault(z => z.Login == tblogin.Text && z.Password == p);
+            if (UsersObject == null)
+            {
+                MessageBox.Show("Не верно введены логин или пароль!");
+            }
+            else
+            {
+				switch (UsersObject.id_role)  
+				{
+					case 2:  // администратор
+						Class1.Mfrm.Navigate(new MainAdmin()); 
+                        MessageBox.Show("Привет, администратор");
+						break;
+					case 1:  // пользователь
+						Class1.Mfrm.Navigate(new MainUser());
+						MessageBox.Show("Привет, пользователь");
+						break;
+					default:
+						MessageBox.Show("Пока");
+						break;
+				}
+            }
+        }
+
+        private void Btn3_Click(object sender, RoutedEventArgs e)
+        {
+            Class1.Mfrm.Navigate(new Registr());
+        }
+
+    }
+
+}
